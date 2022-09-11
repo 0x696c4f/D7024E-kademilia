e3165f8f09bca7beee020ac3692cffd45a6b0713
Alepes-8 <alex.peschel@gmail.com>
2022-09-11 12:55:37 +0000

    establish some more steps and core code

--- src/D7024E/network.go
+++ src/D7024E/network.go
@@ -5,6 +5,7 @@ import (
 )
 
 type Network struct {
+	//TODO
 }
 
 func Listen(ip string, port int) {
@@ -18,16 +19,19 @@ func NewNetwork() *Network {
 
 func (network *Network) SendPingMessage(contact *Contact) {
 	fmt.Println("Sending ping message")
-	//have a massage which is encoded
+	//have a massage which is encoded or set up in some way
 
 	//Send Message
 	/*
 		0: Take out the contact information from contact
-		1: establish connection
+			0.1: Get UDP adress
+		1: establish connection (https://pkg.go.dev/net)
+			Answer: DialUDP(network string, laddr, raddr *UDPAddr)
 			1.1 If connection fail send error message
 			1.2 defer closing the connection. Making sure that it closes even on errors
 		2: Write through connection
 		3: set a deadline for a respons
+			Answer: SetDeadline(t time.Time) error
 		4: If deadline expires send error message
 		5: Message was responed on, add the contact to the bucket
 	*/
