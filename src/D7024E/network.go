acbf34872f8c1ffaa8ab312af1c5ffbb313d3883
Alepes-8 <alex.peschel@gmail.com>
2022-09-11 11:34:27 +0000

    establish the needs in ping

--- src/D7024E/network.go
+++ src/D7024E/network.go
@@ -18,8 +18,19 @@ func NewNetwork() *Network {
 
 func (network *Network) SendPingMessage(contact *Contact) {
 	fmt.Println("Sending ping message")
-	//have a massage which is encoded 	"message := EncodeString("Just a PING message")"
-	//send the message to the dedicated address  "go network.sendUDP("PING", contact.Address, message)"
+	//have a massage which is encoded
+
+	//Send Message
+	/*
+		0: Take out the contact information from contact
+		1: establish connection
+			1.1 If connection fail send error message
+			1.2 defer closing the connection. Making sure that it closes even on errors
+		2: Write through connection
+		3: set a deadline for a respons
+		4: If deadline expires send error message
+		5: Message was responed on, add the contact to the bucket
+	*/
 
 }
 
