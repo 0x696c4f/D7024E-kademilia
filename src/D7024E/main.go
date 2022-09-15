af622c76294671091a1a0d4519fbaa9e9e4678a1
0x696c4f <0x696c4f@gmx.de>
2022-09-15 14:27:24 +0200

    Added command line arg code

--- src/D7024E/main.go
+++ src/D7024E/main.go
@@ -4,11 +4,64 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strconv"
 )
 
+func printHelpExit(msg string) {
+	helpText := "Usage:\nstart [port]\t\t start the first node of a kademlia network\njoin <ip> [port]\t join an existing network using the node ip:port as the entrypoint\nget <hash>\t\t get the object with hash from the network\nput <data>\t\t store data into the network\n\nThe default port always is 4000\n"
+	fmt.Println(msg+"\n\n",helpText)
+	os.Exit(1)
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		printHelpExit("No command supplied.")
+	}
+	port := 4000
+	switch os.Args[1] {
+		case "start" : {
+			// start the network
+			if len(os.Args) >= 3 {
+				var err error 
+				port,err = strconv.Atoi(os.Args[2])
+				if err != nil {
+					printHelpExit("Invalid port.")
+				}
+			}
+			fmt.Println("starting network on port",port)
+		}
+		case "join" : {
+			if len(os.Args) < 3 {
+				printHelpExit("No entrypoint given.")
+			}
+			ipStr := os.Args[2]
+			ip := net.ParseIP(ipStr)
+			if ip == nil {
+				printHelpExit("Invalid IP")
+			}
+			if len(os.Args) >= 4 {
+				var err error 
+				port,err = strconv.Atoi(os.Args[3])
+				if err != nil {
+					printHelpExit("Invalid port.")
+				}
+			}
+			fmt.Println("joining via",ip,":",port)
+		}
+		case "get" : {
+			hash := os.Args[2]
+			fmt.Println("getting ",hash)
+		}
+		case "put" : {
+			data := os.Args[2]
+			fmt.Println("storing ",data)
+		}
+		default:
+			printHelpExit("Invalid command.")
+	}
+
 	fmt.Println("start")
-	port := "8080"
 
 	myIP := GetOutboundIP()
 	localIP := myIP.String() + ":" + port
