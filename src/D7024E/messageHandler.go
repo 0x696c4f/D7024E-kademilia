23a7bb8e492b595aa5086b01acdd9ed20ac49cbe
Alepes-8 <alex.peschel@gmail.com>
2022-09-16 13:22:03 +0000

    change the response to pong to differenceate them

--- src/D7024E/messageHandler.go
+++ src/D7024E/messageHandler.go
@@ -14,7 +14,7 @@ func (network *Network) MessageHandler(message *Packet) Packet {
 
 func (network *Network) NewResponsePacket(message *Packet) (pack Packet) {
 	pack = Packet{
-		RPC:            "ping",     //TODO should it be called the same
+		RPC:            "pong",     //TODO should it be called the same
 		ID:             message.ID, //TODO Should it have it's own or message ID
 		SendingContact: network.node.routingTable.me,
 	}
