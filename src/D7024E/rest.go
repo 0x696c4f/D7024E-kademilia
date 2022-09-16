1673c5d645287d8f63ee82709bac10d5f32155ba
Alepes-8 <alex.peschel@gmail.com>
2022-09-16 14:21:44 +0000

    commenting out for my convience

--- src/D7024E/rest.go
+++ src/D7024E/rest.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 type msg struct {
-    Data     string  `json:"data"`
+	Data string `json:"data"`
 }
+
 // getAlbums responds with the list of all albums as JSON.
 func getObject(c *gin.Context) {
 	hash := c.Param("hash")
 	//TODO: load data
-	data := "data with hash "+hash
+	data := "data with hash " + hash
 
-    var newMsg msg
+	var newMsg msg
 	newMsg.Data = string(data)
 
-    c.IndentedJSON(http.StatusOK, newMsg)
+	c.IndentedJSON(http.StatusOK, newMsg)
 }
 
 func postData(c *gin.Context) {
-    var newMsg msg
+	var newMsg msg
 
-
-	data,_:=c.GetRawData()
+	data, _ := c.GetRawData()
 	newMsg.Data = string(data)
 
 	//TODO: use newMsg to store data, get hash
-	hash:= "abcd"
+	hash := "abcd"
 
-    c.Writer.Header().Set("Location","/objects/"+hash)
+	c.Writer.Header().Set("Location", "/objects/"+hash)
 
-    // Add the new album to the slice.
-    c.IndentedJSON(http.StatusCreated, newMsg)
+	// Add the new album to the slice.
+	c.IndentedJSON(http.StatusCreated, newMsg)
 }
 
 func RestApi() {
-    router := gin.Default()
+	router := gin.Default()
 	router.GET("/objects/:hash", getObject)
-	router.POST("/objects",postData)
+	router.POST("/objects", postData)
 
-    router.Run("localhost:8080")
+	router.Run("localhost:8080")
 }
