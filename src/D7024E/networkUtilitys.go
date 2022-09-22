ee2f0bb8502572490ddacf11a586180d3eacb9ee
Alepes-8 <alex.peschel@gmail.com>
2022-09-22 14:57:05 +0000

    Add functions for the convertion of contact to byte and back

--- src/D7024E/networkUtilitys.go
+++ src/D7024E/networkUtilitys.go
@@ -6,6 +6,17 @@ import (
 	"strconv"
 )
 
+func ContactToByte(con Contact) []byte {
+	message, _ := json.Marshal(con)
+	return message
+}
+
+func ByteToContact(message []byte) Contact {
+	con := Contact{}
+	json.Unmarshal(message, &con)
+	return con
+}
+
 func PacketToByte(pkt Packet) []byte {
 	message, _ := json.Marshal(pkt)
 	return message
