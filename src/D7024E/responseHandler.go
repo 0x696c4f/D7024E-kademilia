c44f3c12d16a8b4fcec860c493f51f7434bf78a3
Alepes-8 <alex.peschel@gmail.com>
2022-09-23 14:45:18 +0000

    Add a handler for the handlefindnode response

--- src/D7024E/responseHandler.go
+++ src/D7024E/responseHandler.go
@@ -1,7 +1,16 @@
 package main
 
+import "fmt"
+
 func (network *Network) ResponseHandler(response *Packet) {
 	if response.RPC == "pong" {
 		network.AddToRoutingTable(response.SendingContact)
+	} else if response.RPC == "find_Node_res" {
+		network.HandleFindNodeResponse(response)
 	}
+}
+
+func (network *Network) HandleFindNodeResponse(response *Packet) {
+	//TODO
+	fmt.Println("handle the find node responce")
 }
