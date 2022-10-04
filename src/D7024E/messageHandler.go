cc9193faf58e69949ff7fb8d8ca8005723707656
oliolo <oliver_olofsson@hotmail.se>
2022-10-04 17:36:57 +0000

    Find_value message should be complete

--- src/D7024E/messageHandler.go
+++ src/D7024E/messageHandler.go
@@ -8,6 +8,8 @@ func (network *Network) MessageHandler(message Packet) Packet {
 		return network.NewPingResponsePacket(message)
 	} else if message.RPC == "find_Node" {
 		return network.NewFindNodeResponsePacket(message)
+	} else if message.RPC == "find_Value" {
+		return network.NewFindValueResponsePacket(message)
 	} else if message.RPC == "store_Value" {
 		return network.NewStoreResponsePacket(message)
 	}
@@ -31,6 +33,39 @@ func (network *Network) NewFindNodeResponsePacket(packMesssage Packet) Packet {
 
 	pack := Packet{
 		RPC:            "find_Node",
+		SendingContact: &network.Node.RoutingTable.me,
+		Message:        response,
+	}
+
+	return pack
+}
+
+func (network *Network) NewFindValueResponsePacket(packMesssage Packet) Packet {
+	//Sends the data object from the map if the hash matches a stored key
+	var value []byte
+	value = network.StoreValues[packMesssage.Message.Hash]
+
+	if value != nil {
+		fmt.Println("The value was found!! ", value)
+		response := MessageBody{
+			Data: value,
+		}
+
+		pack := Packet{
+			RPC:            "find_Value",
+			SendingContact: &network.Node.RoutingTable.me,
+			Message:        response,
+		}
+		return pack
+	}
+
+	fmt.Println("The value was not found!! ", value)
+	response := MessageBody{
+		ContactList: network.Node.RoutingTable.FindClosestContacts(packMesssage.Message.TargetID, network.Node.Alpha),
+	}
+
+	pack := Packet{
+		RPC:            "find_Value",
 		SendingContact: &network.Node.RoutingTable.me,
 		Message:        response,
 	}
